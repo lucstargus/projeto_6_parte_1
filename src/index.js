57945036661e145d1e9322a51a50db40800f4ba1
Lucas <158581359+lucstargus@users.noreply.github.com>
2026-01-22 16:04:57 -0300

    Add comments to index.js imports
    Added comments to imports for better clarity.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import "./index.css"; // Arquivo CSS global
+import App from "./App"; // Componente principal do App
+import { BrowserRouter } from "react-router-dom"; // Para navegação com React Router
 
 ReactDOM.render(
   <BrowserRouter>
