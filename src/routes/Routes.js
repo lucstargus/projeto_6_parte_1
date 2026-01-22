1bd459c824f6f9f67d690a820a72bd1706e1019b
Lucas <158581359+lucstargus@users.noreply.github.com>
2026-01-22 18:36:31 +0000

    inclusão

---
+++ src/routes/Routes.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import Menu from "../pages/Menu";
+import Contact from "../pages/Contact";
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/menu" element={<Menu />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  );
+}
+
+export default AppRoutes;
