c888877f2160d3a87316126fc3e30272a4e45ca9
Lucas <158581359+lucstargus@users.noreply.github.com>
2026-01-22 16:16:31 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import Contact from "./pages/Contact";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Footer() {
+  return <footer>© 2023 Efood</footer>;
+}
+
+export default Footer;
